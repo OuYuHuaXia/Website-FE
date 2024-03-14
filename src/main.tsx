adbc27fd85119dd23644f66907253062661c296d
墨綠BG <s5460703@gmail.com>
2024-03-14 12:55:58 +0800

    🔧 chore：使用 Sass

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 import App from "./App";
-import "./globals.css";
+import "./globals.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
